4dff602b2717994bfba9c958418c889d2c83398e
remixj <jianghongxiang@bupt.edu.cn>
2019-10-03 10:13:16 -0700

    2019-10-03 add html

---
+++ Insta/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include , path
+
+from Insta.views import HelloWorld
+
+urlpatterns = {
+    path('', HelloWorld.as_view() , name = "HellowWorld"),
+}
+
